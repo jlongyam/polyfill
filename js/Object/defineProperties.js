2cd6dad97fa664e0b5551220476933c7d119a96e
jlongyam <jhonliongbox@gmail.com>
2026-02-15 09:52:47 +0700

    fix: Object.defineProperties

--- js/Object/defineProperties.js
+++ js/Object/defineProperties.js
@@ -1,12 +1,10 @@
-if (typeof Object.defineProperties !== "function") {
-  Object.defineProperties = function (o, properties) {
-    if (o !== Object(o)) { throw TypeError("Object.defineProperties called on non-object"); }
-    var name;
-    for (name in properties) {
-      if (Object.prototype.hasOwnProperty.call(properties, name)) {
-        Object.defineProperty(o, name, properties[name]);
+if (!Object.defineProperties) {
+  Object.defineProperties = function(obj, props) {
+    for (var prop in props) {
+      if (Object.prototype.hasOwnProperty.call(props, prop)) {
+        Object.defineProperty(obj, prop, props[prop]);
       }
     }
-    return o;
+    return obj;
   };
 }
--- js/Object/defineProperty.js
+++ js/Object/defineProperty.js
@@ -1,19 +1,8 @@
-(function () {
-  if (typeof Object.defineProperty !== 'function') {
-    Object.defineProperty = function (obj, prop, descriptor) {
-      if (obj !== Object(obj)) {
-        throw new TypeError('Object.defineProperty called on non-object');
-      }
-      // if (descriptor.get && obj.__defineGetter__) {
-      //   obj.__defineGetter__(prop, descriptor.get);
-      // }
-      // if (descriptor.set && obj.__defineSetter__) {
-      //   obj.__defineSetter__(prop, descriptor.set);
-      // }
-      if ('value' in descriptor && !descriptor.get && !descriptor.set) {
-        obj[prop] = descriptor.value;
-      }
-      return obj;
-    };
-  }
-})();
+if (!Object.defineProperty) {
+  Object.defineProperty = function(obj, prop, descriptor) {
+    if ('value' in descriptor) {
+      obj[prop] = descriptor.value;
+    }
+    return obj;
+  };
+}
